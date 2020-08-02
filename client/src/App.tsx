5332ca7365294ed242c0108648bd1bd11b04cba2
Ibraheem Ahmed <ibrah1440@gmail.com>
2020-08-02 18:42:02 -0400

    convert input state to object

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 
 const App = () => {
   let phrase = "Help us decide ";
-  type status = "has-input" | "has-focus" | "idle";
+
+  type status = "has-poll" | "has-focus" | "idle";
   const [status, setStatus] = useState<status>("idle");
-  const inputArray = [
+
+  const pollArray = [
     "where to go this evening",
     "what movie to watch",
     "what to eat",
@@ -12,35 +14,42 @@ const App = () => {
     "what to wear to the party",
     "what time to meet up",
   ];
-  const [input, setInput] = useState<string>(
-    phrase + inputArray[Math.floor(Math.random() * inputArray.length)]
-  );
+
+  interface Poll {
+    title: string;
+  }
+  const [poll, setPoll] = useState<Poll>({
+    title:
+      phrase + pollArray[Math.floor(Math.random() * pollArray.length)],
+  });
+
   useEffect(() => {
     const timeout = setInterval(() => {
       while (status === "idle") {
-        let nxt = Math.floor(Math.random() * inputArray.length);
-        if (inputArray[nxt] !== input) {
-          setInput(phrase + inputArray[nxt]);
+        let nxt = Math.floor(Math.random() * pollArray.length);
+        if (pollArray[nxt] !== poll.title) {
+          setPoll({...poll, title: phrase + pollArray[nxt]});
           break;
         }
       }
     }, 2000);
     return () => clearInterval(timeout);
-  }, [input, inputArray]);
+  }, [poll, pollArray]);
+  
   return (
     <div className="font-nunito">
       <div className="mx-6">
         <input
           type="text"
-          value={input}
+          value={poll.title}
           onFocus={() => {
-            setStatus("has-input");
-            setInput(phrase);
+            setStatus("has-poll");
+            setPoll({...poll, title: phrase});
           }}
-          onBlur={() => input === phrase && setStatus("idle")}
+          onBlur={() => poll.title === phrase && setStatus("idle")}
           onChange={(event) => {
-            setInput(event.target.value);
-            input === "phrase" ? setStatus("idle") : setStatus("has-input");
+            setPoll({...poll, title: event.target.value});
+            poll.title === "phrase" ? setStatus("idle") : setStatus("has-poll");
           }}
           className="shadow-inner w-full py-3 mx-auto mt-64 text-center text-2xl placeholder-gray-500 text-gray-600 font-semibold tracking-tight border border-gray-500 rounded-lg focus:outline-none focus:border-blue-400"
         />
