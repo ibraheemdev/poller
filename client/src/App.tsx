213418d466b19b68692c02629b257858472cdab8
Ibraheem Ahmed <ibrah1440@gmail.com>
2020-08-02 18:09:55 -0400

    text changing input

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-  return <div className="bg-gray-600">Tailwindcss!!!</div>;
+  let phrase = "Help us decide ";
+  type status = "has-input" | "has-focus" | "idle";
+  const [status, setStatus] = useState<status>("idle");
+  const inputArray = [
+    "where to go this evening",
+    "what movie to watch",
+    "what to eat",
+    "whatever",
+    "what to wear to the party",
+    "what time to meet up",
+  ];
+  const [input, setInput] = useState<string>(
+    phrase + inputArray[Math.floor(Math.random() * inputArray.length)]
+  );
+  useEffect(() => {
+    const timeout = setInterval(() => {
+      while (status === "idle") {
+        let nxt = Math.floor(Math.random() * inputArray.length);
+        if (inputArray[nxt] !== input) {
+          setInput(phrase + inputArray[nxt]);
+          break;
+        }
+      }
+    }, 2000);
+    return () => clearInterval(timeout);
+  }, [input, inputArray]);
+  return (
+    <div className="font-nunito">
+      <div className="mx-6">
+        <input
+          type="text"
+          value={input}
+          onFocus={() => {
+            setStatus("has-input");
+            setInput(phrase);
+          }}
+          onBlur={() => input === phrase && setStatus("idle")}
+          onChange={(event) => {
+            setInput(event.target.value);
+            input === "phrase" ? setStatus("idle") : setStatus("has-input");
+          }}
+          className="shadow-inner w-full py-3 mx-auto mt-64 text-center text-2xl placeholder-gray-500 text-gray-600 font-semibold tracking-tight border border-gray-500 rounded-lg focus:outline-none focus:border-blue-400"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './src/**/*.jsx',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'nunito': 'Nunito, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;'
+      }
+    },
   },
   variants: {},
   plugins: [],
