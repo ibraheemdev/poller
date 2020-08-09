f7f01bd06eadcd8437ac3d9448bde430500df21d
Ibraheem Ahmed <ibrah1440@gmail.com>
2020-08-09 19:05:50 -0400

    validator test

---
+++ pkg/validator/validator_test.go
@@ -0,0 +1,49 @@
+package validator
+
+import (
+	"log"
+	"testing"
+)
+
+func TestValidate(t *testing.T) {
+	v := &Validator{}
+	v.Validate(true == true, "true shouldn't equal true")
+	if v.Errors == nil {
+		t.Error("expected validation error")
+	}
+}
+
+func TestStringify(t *testing.T) {
+	v := &Validator{}
+	v.ValidatePresenceOf("name", "")
+	v.ValidatePresenceOf("name", "")
+	msgs := Stringify(v.Errors)
+	if len(msgs) != 2 {
+		t.Errorf("expected 2 validation errors, got %v", len(msgs))
+	}
+}
+
+func TestValidatePresenceOf(t *testing.T) {
+	v := &Validator{}
+	v.ValidatePresenceOf("name", "")
+	if v.Errors == nil {
+		t.Error("expected validation error for blank field")
+	}
+}
+
+func TestValidateMaxLengthOf(t *testing.T) {
+	v := &Validator{}
+	v.ValidateMaxLengthOf("name", "aaa", 2)
+	log.Println(v.Errors)
+	if v.Errors == nil {
+		t.Error("expected validation error for long field")
+	}
+}
+
+func TestValidateMinLengthOf(t *testing.T) {
+	v := &Validator{}
+	v.ValidateMinLengthOf("name", "a", 2)
+	if v.Errors == nil {
+		t.Error("expected validation error for short field")
+	}
+}
